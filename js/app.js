d4adb2590ad6fc9a93a998be28b91f65bcbb7614
unknown <huynhjjk@yahoo.com>
2014-03-13 23:27:19 -0700

    final source code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,82 @@
-var app = angular.module('Glaciers', []);
+var app = angular.module('HCDE411', ['ui.bootstrap']);
 
 /* In case if there are any cross-origin browser issues */
 app.config(['$httpProvider', function($httpProvider) {  
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
+
+  app.directive('documentSwitchOff', [
+    '$parse',
+    '$timeout',
+    function ($parse,$timeout) {
+        return function (scope, element, attrs) {
+            var getter = $parse(attrs.documentSwitchOff);
+            var setter = getter.assign;
+            var clickInsideElement = false;
+            function elementClickHandler(){
+                clickInsideElement = true;
+            }
+            function documentClickHandler(){
+                if(!clickInsideElement){
+                    scope.$apply(function(){
+                        setter(scope,false);
+                    });
+                }
+                clickInsideElement = false;
+            }
+            var bound = false;
+            scope.$watch(attrs.documentSwitchOff,function(newVal){
+                if(angular.isDefined(newVal)){
+                    if(newVal){
+                        $timeout(function(){
+                            bound = true;
+                            element.bind("click",elementClickHandler);
+                            var doc = angular.element(document)
+                                doc.bind('click',documentClickHandler);
+                        },0);
+                    }
+                    else{
+                        if(bound){
+                            element.unbind("click",elementClickHandler);
+                            angular.element(document).unbind('click',documentClickHandler);
+                            bound = false;
+                        }
+
+                    }
+                }
+            });
+
+            scope.$on("$destroy",function(){
+                if(bound){
+                    element.unbind("click",elementClickHandler);
+                    angular.element(document).unbind('click',documentClickHandler);
+                    bound = false;
+                }
+            });
+        }
+    }
+]);
+
+app.filter('myFilter', function() {
+     return function(markets, range) {
+      var out = [];
+      // console.log(range)
+      
+      angular.forEach(markets, function(Mitem, Mindex){
+        var flag = true;
+        angular.forEach(range, function(Ritem, Rindex){
+          if(!Ritem) return;
+          MitemBoolVal = (Mitem[Rindex] == "Y");
+          flag = ((MitemBoolVal == Ritem && flag)); 
+        });
+        
+        if(flag){
+          out.push(Mitem);
+        }
+      });
+      
+      // console.log(out);
+      return out;
+    }
+  });
--- js/controllers.js
+++ js/controllers.js
@@ -1,135 +1,258 @@
 /* Retrieving data */
-function DataCtrl($scope) {
-  $scope.data = [
-    {
-      name: "Glacier #1",
-      lat: 37,
-      lng: -122,
-      rows: [['Mushrooms', 3],['Onions', 1],['Olives', 1],['Zucchini', 1], ['Pepperoni', 2]]
-    },
-    {
-      name: "Glacier #2",
-      lat: 38,
-      lng: -123,
-      rows: [['Mushrooms', 1],['Onions', 7],['Olives', 2],['Zucchini', 3], ['Pepperoni', 5]]
-    },
-    {
-      name: "Glacier #3",
-      lat: 39,
-      lng: -124,
-      rows: [['Mushrooms', 20],['Onions', 40],['Olives', 60],['Zucchini', 10], ['Pepperoni', 8]]
-    },
-    {
-      name: "Glacier #4",
-      lat: 40,
-      lng: -125,
-      rows: [['Mushrooms', 321],['Onions', 217],['Olives', 21],['Zucchini', 311], ['Pepperoni', 200]]
-    },    
-    {
-      name: "Seattle",
-      lat: 47.60621,
-      lng: -122.332071,
-      rows: [['Mushrooms', 19],['Onions', 71],['Olives', 27],['Zucchini', 32], ['Pepperoni', 56]]      
-    },
-    {
-      name: "UW",
-      lat: 47.655335,
-      lng: -122.303519,
-      rows: [['Mushrooms', 100],['Onions', 200],['Olives', 400],['Zucchini', 400], ['Pepperoni', 500]]
+function DataCtrl($scope, $http, $q, $filter) {
+ $scope.$watchCollection('filtered', function(fmData) {
+    $scope.statesCount = {};
+    var filtered = $filter('myFilter')(fmData);
+    for (var i = 0; i < filtered.length; i++) {
+        if ($scope.statesCount[filtered[i].State]) {
+         $scope.statesCount[filtered[i].State]++;    
+        } else {
+         $scope.statesCount[filtered[i].State] = 1;
+        }
     }
-  ];
+    $scope.initialize(fmData);
+  });
+ 
+  $scope.$on('LOAD', function() {
+      $scope.loading = true;
+  });
+  $scope.$on('DONE', function() {
+      $scope.loading = false;
+  });
+  $scope.$emit('LOAD');
+  $scope.orderByField = 'MarketName';
+  $scope.reverseSort = false;
+  $scope.details = undefined;
+  $scope.state = undefined;
+  $scope.map = undefined;
+  $scope.mapOptions = undefined;
+  $scope.lat = 41.5672;
+  $scope.lng = -122.1269;
+  $scope.zoom = 5;
 
-
-  // EARTH //
-
-  /* Call initalization function when the page is loaded*/
-  google.setOnLoadCallback(init);
-
-  var ge;
-
-  /* Initialize Earth */
-  function init() {
-    google.earth.createInstance('map3d', initCB, failureCB);
+  $scope.onSelect = function(state) {
+    $scope.state = state;
+    for (var i = 0; i < $scope.states.length; i++) {
+      if ($scope.states[i].state == $scope.state) {
+        $scope.lat = $scope.states[i].latitude;
+        $scope.lng = $scope.states[i].longtitude;
+        var myLatlng = new google.maps.LatLng($scope.lat, $scope.lng);
+        $scope.zoom = 6;
+        $scope.map.setZoom($scope.zoom);
+        $scope.map.setCenter(myLatlng);
+      }
+    }        
   }
 
-  /* Success callback */
-  function initCB(instance) {
-    ge = instance;
-    ge.getWindow().setVisibility(true);
+  $scope.setSelected = function() {
+     if ($scope.lastSelected) {
+       $scope.lastSelected.selected = '';
+     }
+     this.selected = 'active';
+     $scope.lastSelected = this;
+  }
 
-    // add a navigation control
-    ge.getNavigationControl().setVisibility(ge.VISIBILITY_AUTO);
-
-    // add some layers
-    // ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);
-    // ge.getLayerRoot().enableLayerById(ge.LAYER_ROADS, true);
-
-    for (var i = 0; i < $scope.data.length; i++){
-      createPlacemark($scope.data[i].name, $scope.data[i].lat, $scope.data[i].lng);
+  var deferred = $q.defer();
+  $http.get('json/farmers.json').success(function(data) {
+    $scope.fmData = [];
+    $scope.states = [
+      {"state":"Washington", "latitude":47.3917, "longtitude":-121.5708}, 
+      {"state":"California", "latitude":36.1700, "longtitude":-119.7462},
+      {"state":"Oregon", "latitude":44.5672, "longtitude":-122.1269}      
+    ];
+    for (var i = 0; i < data.length; i++){
+      if (data[i].State == 'Washington' || data[i].State == 'California' || data[i].State == 'Oregon') {
+        // if($scope.states.indexOf(data[i].State) == -1 && data[i].State != null){      
+        //   $scope.states.push(data[i].State);
+        // }
+        $scope.fmData.push(data[i]);
+      }
     }
+    deferred.resolve(); 
+  });
 
-    document.getElementById('installed-plugin-version').innerHTML = ge.getPluginVersion().toString();
+  deferred.promise.then(function() {
+    $scope.$emit('DONE');
+    google.maps.event.addDomListener(window, 'load', $scope.initialize());
+  });
+
+  $scope.getDetails = function (farmer) {
+    $scope.lat = farmer.x;
+    $scope.lng = farmer.y;
+    $scope.details = farmer;    
+    var myLatlng = new google.maps.LatLng($scope.lat,$scope.lng);
+    $scope.zoom = 12;
+    $scope.map.setZoom($scope.zoom);
+    $scope.map.setCenter(myLatlng);
+  };
+
+  $scope.initialize = function(fmData) {
+    // Create an array of styles.
+    // var styles = [
+
+    // {
+    //   "featureType": "water",
+    //   "stylers": [
+    //     { "color": "#4155c5" }
+    //   ]
+    // },{
+    //   "featureType": "landscape",
+    //   "stylers": [
+    //     { "color": "#7c9b24" }
+    //   ]
+    // },{
+    //   "featureType": "road",
+    //   "stylers": [
+    //     { "color": "#ebbf42" }
+    //   ]
+    // },
+
+    // {
+    //   "featureType": "administrative.neighborhood",
+    //   "stylers": [
+    //     { "visibility": "off" }
+    //   ]
+    // },{
+    //   "featureType": "administrative.locality",
+    //   "stylers": [
+    //     { "visibility": "off" }
+    //   ]
+    // },{
+    //   "featureType": "poi",
+    //   "stylers": [
+    //     { "visibility": "off" }
+    //   ]
+    // },{
+    //   "featureType": "transit.line",
+    //   "stylers": [
+    //     { "visibility": "off" }
+    //   ]
+    // },{
+    //   "featureType": "road",
+    //   "stylers": [
+    //     { "visibility": "simplified" }
+    //   ]
+    // },{
+    //   "elementType": "labels",
+    //   "stylers": [
+    //     { "visibility": "simplified" }
+    //   ]
+    // }];
+
+  // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  // var styledMap = new google.maps.StyledMapType(styles,
+  //   {name: "Farmers Markets"});
+
+    $scope.mapOptions = {
+      zoom: $scope.zoom,
+      center: new google.maps.LatLng($scope.lat, $scope.lng)
+      // ,mapTypeControlOptions: {
+      //   mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      // }        
+      // center: new google.maps.LatLng(37.09024, -95.712891)
+    }
+    if (fmData) {
+    var image = new google.maps.MarkerImage(
+      'img/marker.png',
+      null, /* size is determined at runtime */
+      null, /* origin is 0,0 */
+      null, /* anchor is bottom center of the scaled image */
+      new google.maps.Size(7, 7)
+    );  
+      $scope.map = new google.maps.Map(document.getElementById('map-canvas'), $scope.mapOptions);
+
+      //Associate the styled map with the MapTypeId and set it to display.
+      // $scope.map.mapTypes.set('map_style', styledMap);
+      // $scope.map.setMapTypeId('map_style');
+
+      $scope.drawChart($scope.statesCount['Washington'], $scope.statesCount['Oregon'], $scope.statesCount['California']);
+      for (var i = 0; i < fmData.length; i++){
+          var myLatlng = new google.maps.LatLng(fmData[i].x,fmData[i].y);
+            var marker = new google.maps.Marker({
+              position: myLatlng,
+              map: $scope.map,
+              title: fmData[i].MarketName,
+              icon: image
+              // ,icon: $scope.getCircle()
+          });
+          $scope.setMarker($scope.map, marker, fmData[i]);
+      }    
+      google.maps.event.addListener($scope.map, 'center_changed', function() {
+        $scope.lat = $scope.map.getCenter().lat();
+        $scope.lng = $scope.map.getCenter().lng();
+      });
+      google.maps.event.addListener($scope.map, 'zoom_changed', function() {
+        $scope.zoom = $scope.map.getZoom();
+      });
+    }
   }
+  $scope.setMarker = function (map, marker, farmer) {
+    var infowindow = new google.maps.InfoWindow({
+      content:  
+                '<h3>' + farmer.MarketName + '</h3>' + 
+                '<br>' + 
+                '<b>Address:</b> ' + farmer.street + ' ' +farmer.city + ', ' + farmer.State + ' ' + farmer.zip +
+                '<br>' +
+                '<b>Website:</b> ' + '<a href=' + '"'+ farmer.Website + '"' + ' target="_blank">' + farmer.Website + '</a>'
 
-  /* Failure callback */
-  function failureCB(errorCode) {
+    });
+   google.maps.event.addListener(marker, 'click', function() {
+      $scope.zoom = 12;
+      map.setZoom($scope.zoom);
+      map.setCenter(marker.getPosition());
+      infowindow.open(marker.get('map'), marker);
+     $scope.$apply(function() {
+         $scope.details = farmer;
+     });
+
+    });
   }
-
-  function createPlacemark(name, lat, lng) { 
-    // Create point
-    var la = ge.getView().copyAsLookAt(ge.ALTITUDE_RELATIVE_TO_GROUND);
-    var point = ge.createPoint('');
-    point.setLatitude(lat);
-    point.setLongitude(lng);
-
-    var placemark = ge.createPlacemark('');
-    placemark.setName(name);
-    placemark.setDescription('<a href="https://catalyst.uw.edu/workspace/omni/42527/302765"></a>' + '<br>' + 'The latitude is: ' + lat + '<br>' + 'The longtitude is: ' + lng);
-    ge.getFeatures().appendChild(placemark);
-
-    // Create style map for placemark
-    var icon = ge.createIcon('');
-    icon.setHref('http://maps.google.com/mapfiles/kml/paddle/red-circle.png');
-    var style = ge.createStyle('');
-    style.getIconStyle().setIcon(icon);
-    style.getIconStyle().setScale(5.0);  
-    placemark.setStyleSelector(style);
-
-    placemark.setGeometry(point);
+  $scope.getCircle = function() {
+    var circle = {
+      path: google.maps.SymbolPath.CIRCLE,
+      fillColor: '#c10d18',
+      fillOpacity: .5,
+      scale: 6,
+      strokeColor: '#ffffff',
+      strokeWeight: .5
+    };
+    return circle;
   }
+  $scope.drawChart = function(washington, oregon, california) {
+      if (washington == undefined) {
+        washington = 0
+      }
+      if (oregon == undefined) {
+        oregon = 0
+      }
+      if (california == undefined) {
+        california = 0
+      }
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Farmers Markets", { role: "style" } ],
+        ["Washington", washington, "#418bca"],
+        ["Oregon", oregon, "#014a01"],
+        ["California", california, "#fed700"]
+      ]);
 
-  $scope.getView = function(lat, lng) { 
-    // look at the placemark we created
-    var la = ge.createLookAt('');
-    la.set(lat, lng,
-      0, // altitude
-      ge.ALTITUDE_RELATIVE_TO_GROUND,
-      0, // heading
-      0, // straight-down tilt
-      50000 // range (inverse of zoom)
-      );
-    ge.getView().setAbstractView(la);    
-  }
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+         { calc: "stringify",
+           sourceColumn: 1,
+           type: "string",
+           role: "annotation" },
+       2]);
 
-
-  // CIRCLE //
-
-  // Set a callback to run when the Google Visualization API is loaded.
-  $scope.getCircle = function(lat, lng, rows) {
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows(rows);
-
-    // Set chart options
-    var options = {'title':'How Much Pizza I Ate Last Night',
-                   'width':400,
-                   'height':300};
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
+      var options = {
+        title: "Farmers Markets by State",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+    chart.draw(view, options);
   }
 
 }
